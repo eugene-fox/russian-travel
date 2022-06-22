11d2b5a415d0f65bbd1ece46b0abea45b5603cf4
eugene-fox <69255033+eugene-fox@users.noreply.github.com>
2022-06-22 16:17:45 +0700

    Updates

--- main.js
+++ main.js
@@ -7,8 +7,17 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/favicons/favicons.js":
+/*!**********************************!*\
+  !*** ./src/favicons/favicons.js ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("var faviconsContext = __webpack_require__(\"./src/favicons sync recursive !!./node_modules/file-loader/dist/cjs.js?name=[name].[ext]! \\\\.(svg%7Cpng%7Cico%7Cxml%7Cjson)$\");\n\nfaviconsContext.keys().forEach(faviconsContext);\n\n//# sourceURL=webpack://russian-travel/./src/favicons/favicons.js?");
+
+/***/ }),
 
 /***/ "./src/index.js":
 /*!**********************!*\
@@ -16,7 +25,30 @@
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _pages_index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./pages/index.css */ \"./src/pages/index.css\");\n // добавьте импорт главного файла стилей\n\n//# sourceURL=webpack://russian-travel/./src/index.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _favicons_favicons__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./favicons/favicons */ \"./src/favicons/favicons.js\");\n/* harmony import */ var _favicons_favicons__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_favicons_favicons__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _pages_index_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./pages/index.css */ \"./src/pages/index.css\");\n\n // добавьте импорт главного файла стилей\n\n//# sourceURL=webpack://russian-travel/./src/index.js?");
+
+/***/ }),
+
+/***/ "./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/browserconfig.xml":
+/*!*************************************************************************************************!*\
+  !*** ./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/browserconfig.xml ***!
+  \*************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"browserconfig.xml\");\n\n//# sourceURL=webpack://russian-travel/./src/favicons/browserconfig.xml?./node_modules/file-loader/dist/cjs.js?name=%5Bname%5D.%5Bext%5D");
+
+/***/ }),
+
+/***/ "./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/favicon.ico":
+/*!*******************************************************************************************!*\
+  !*** ./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/favicon.ico ***!
+  \*******************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"favicon.ico\");\n\n//# sourceURL=webpack://russian-travel/./src/favicons/favicon.ico?./node_modules/file-loader/dist/cjs.js?name=%5Bname%5D.%5Bext%5D");
 
 /***/ }),
 
@@ -26,7 +58,139 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _pag
   \*****************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://russian-travel/./src/pages/index.css?");
+
+/***/ }),
+
+/***/ "./src/favicons sync recursive !!./node_modules/file-loader/dist/cjs.js?name=[name].[ext]! \\.(svg%7Cpng%7Cico%7Cxml%7Cjson)$":
+/*!**************************************************************************************************************************!*\
+  !*** ./src/favicons/ sync !!./node_modules/file-loader/dist/cjs.js?name=[name].[ext]! \.(svg%7Cpng%7Cico%7Cxml%7Cjson)$ ***!
+  \**************************************************************************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("var map = {\n\t\"./android-chrome-192x192.png\": \"./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/android-chrome-192x192.png\",\n\t\"./android-chrome-512x512.png\": \"./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/android-chrome-512x512.png\",\n\t\"./apple-touch-icon.png\": \"./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/apple-touch-icon.png\",\n\t\"./browserconfig.xml\": \"./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/browserconfig.xml\",\n\t\"./favicon-16x16.png\": \"./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/favicon-16x16.png\",\n\t\"./favicon-32x32.png\": \"./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/favicon-32x32.png\",\n\t\"./favicon.ico\": \"./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/favicon.ico\",\n\t\"./mstile-144x144.png\": \"./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/mstile-144x144.png\",\n\t\"./mstile-150x150.png\": \"./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/mstile-150x150.png\",\n\t\"./mstile-310x150.png\": \"./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/mstile-310x150.png\",\n\t\"./mstile-310x310.png\": \"./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/mstile-310x310.png\",\n\t\"./mstile-70x70.png\": \"./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/mstile-70x70.png\",\n\t\"./safari-pinned-tab.svg\": \"./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/safari-pinned-tab.svg\"\n};\n\n\nfunction webpackContext(req) {\n\tvar id = webpackContextResolve(req);\n\treturn __webpack_require__(id);\n}\nfunction webpackContextResolve(req) {\n\tif(!__webpack_require__.o(map, req)) {\n\t\tvar e = new Error(\"Cannot find module '\" + req + \"'\");\n\t\te.code = 'MODULE_NOT_FOUND';\n\t\tthrow e;\n\t}\n\treturn map[req];\n}\nwebpackContext.keys = function webpackContextKeys() {\n\treturn Object.keys(map);\n};\nwebpackContext.resolve = webpackContextResolve;\nmodule.exports = webpackContext;\nwebpackContext.id = \"./src/favicons sync recursive !!./node_modules/file-loader/dist/cjs.js?name=[name].[ext]! \\\\.(svg%7Cpng%7Cico%7Cxml%7Cjson)$\";\n\n//# sourceURL=webpack://russian-travel/_\\.(svg%257Cpng%257Cico%257Cxml%257Cjson)$?./src/favicons/_sync_!!./node_modules/file-loader/dist/cjs.js?name=%5Bname%5D.%5Bext%5D");
+
+/***/ }),
+
+/***/ "./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/android-chrome-192x192.png":
+/*!**********************************************************************************************************!*\
+  !*** ./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/android-chrome-192x192.png ***!
+  \**********************************************************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+eval("module.exports = __webpack_require__.p + \"6bfae74df8074927e1e1.png\";\n\n//# sourceURL=webpack://russian-travel/./src/favicons/android-chrome-192x192.png?./node_modules/file-loader/dist/cjs.js?name=%5Bname%5D.%5Bext%5D");
+
+/***/ }),
+
+/***/ "./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/android-chrome-512x512.png":
+/*!**********************************************************************************************************!*\
+  !*** ./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/android-chrome-512x512.png ***!
+  \**********************************************************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+eval("module.exports = __webpack_require__.p + \"7f265684f8d2a56503d3.png\";\n\n//# sourceURL=webpack://russian-travel/./src/favicons/android-chrome-512x512.png?./node_modules/file-loader/dist/cjs.js?name=%5Bname%5D.%5Bext%5D");
+
+/***/ }),
+
+/***/ "./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/apple-touch-icon.png":
+/*!****************************************************************************************************!*\
+  !*** ./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/apple-touch-icon.png ***!
+  \****************************************************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+eval("module.exports = __webpack_require__.p + \"87f8b168dfe88d6f1528.png\";\n\n//# sourceURL=webpack://russian-travel/./src/favicons/apple-touch-icon.png?./node_modules/file-loader/dist/cjs.js?name=%5Bname%5D.%5Bext%5D");
+
+/***/ }),
+
+/***/ "./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/favicon-16x16.png":
+/*!*************************************************************************************************!*\
+  !*** ./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/favicon-16x16.png ***!
+  \*************************************************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+eval("module.exports = __webpack_require__.p + \"943c33b99a8e26fe9139.png\";\n\n//# sourceURL=webpack://russian-travel/./src/favicons/favicon-16x16.png?./node_modules/file-loader/dist/cjs.js?name=%5Bname%5D.%5Bext%5D");
+
+/***/ }),
+
+/***/ "./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/favicon-32x32.png":
+/*!*************************************************************************************************!*\
+  !*** ./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/favicon-32x32.png ***!
+  \*************************************************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+eval("module.exports = __webpack_require__.p + \"57683d8586d33862fc64.png\";\n\n//# sourceURL=webpack://russian-travel/./src/favicons/favicon-32x32.png?./node_modules/file-loader/dist/cjs.js?name=%5Bname%5D.%5Bext%5D");
+
+/***/ }),
+
+/***/ "./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/mstile-144x144.png":
+/*!**************************************************************************************************!*\
+  !*** ./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/mstile-144x144.png ***!
+  \**************************************************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+eval("module.exports = __webpack_require__.p + \"e1ecbb020f43d50f6bec.png\";\n\n//# sourceURL=webpack://russian-travel/./src/favicons/mstile-144x144.png?./node_modules/file-loader/dist/cjs.js?name=%5Bname%5D.%5Bext%5D");
+
+/***/ }),
+
+/***/ "./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/mstile-150x150.png":
+/*!**************************************************************************************************!*\
+  !*** ./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/mstile-150x150.png ***!
+  \**************************************************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+eval("module.exports = __webpack_require__.p + \"7d1f606533e1dc9de27e.png\";\n\n//# sourceURL=webpack://russian-travel/./src/favicons/mstile-150x150.png?./node_modules/file-loader/dist/cjs.js?name=%5Bname%5D.%5Bext%5D");
+
+/***/ }),
+
+/***/ "./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/mstile-310x150.png":
+/*!**************************************************************************************************!*\
+  !*** ./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/mstile-310x150.png ***!
+  \**************************************************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+eval("module.exports = __webpack_require__.p + \"84230b7ba96644082642.png\";\n\n//# sourceURL=webpack://russian-travel/./src/favicons/mstile-310x150.png?./node_modules/file-loader/dist/cjs.js?name=%5Bname%5D.%5Bext%5D");
+
+/***/ }),
+
+/***/ "./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/mstile-310x310.png":
+/*!**************************************************************************************************!*\
+  !*** ./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/mstile-310x310.png ***!
+  \**************************************************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+eval("module.exports = __webpack_require__.p + \"7cd375b568740da2c95e.png\";\n\n//# sourceURL=webpack://russian-travel/./src/favicons/mstile-310x310.png?./node_modules/file-loader/dist/cjs.js?name=%5Bname%5D.%5Bext%5D");
+
+/***/ }),
+
+/***/ "./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/mstile-70x70.png":
+/*!************************************************************************************************!*\
+  !*** ./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/mstile-70x70.png ***!
+  \************************************************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+eval("module.exports = __webpack_require__.p + \"2188d893637a0752a6df.png\";\n\n//# sourceURL=webpack://russian-travel/./src/favicons/mstile-70x70.png?./node_modules/file-loader/dist/cjs.js?name=%5Bname%5D.%5Bext%5D");
+
+/***/ }),
+
+/***/ "./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/safari-pinned-tab.svg":
+/*!*****************************************************************************************************!*\
+  !*** ./node_modules/file-loader/dist/cjs.js?name=[name].[ext]!./src/favicons/safari-pinned-tab.svg ***!
+  \*****************************************************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+eval("module.exports = __webpack_require__.p + \"25ea8629a265fdabc881.svg\";\n\n//# sourceURL=webpack://russian-travel/./src/favicons/safari-pinned-tab.svg?./node_modules/file-loader/dist/cjs.js?name=%5Bname%5D.%5Bext%5D");
 
 /***/ })
 
@@ -57,6 +221,35 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	(() => {
 /******/ 		// define __esModule on exports
@@ -66,6 +259,11 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		__webpack_require__.p = "";
 /******/ 	})();
 /******/ 	
 /************************************************************************/
